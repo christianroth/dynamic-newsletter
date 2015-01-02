8fb866a774f87d10f63e070d5fb3942fbc7e4ab1
christianroth <christian.roth@gmail.com>
2015-01-02 15:36:25 -0800

    More

---
+++ newsletter/index.js
@@ -0,0 +1,31 @@
+var Newsletter = function () {
+	this.data = {
+		news_snapshot_title1: null,
+		news_snapshot_summary1: null,
+		news_snapshot_title2: null,
+		news_snapshot_summary2: null,
+		news_snapshot_title3: null,
+		news_snapshot_summary3: null
+	};
+
+	this.fill = function (info) {
+		for(var prop in this.data) {
+			if(this.data[prop] !== 'undefined') {
+				this.data[prop] = info[prop];
+			}
+		}
+	};
+
+
+	this.getInformation = function () {
+		return this.data;
+	};
+};
+
+module.exports = function (info) {
+	var instance = new Newsletter();
+
+	instance.fill(info);
+
+	return instance;
+};
